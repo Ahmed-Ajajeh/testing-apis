9fd29bb924188904f35680a5d235e20dd9bb6b14
Ahmad-Ajajeh <secretajajeh356@gmail.com>
2024-06-11 17:44:38 +0300

    starting

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send("hello, world");
+});
+
+app.listen(8888, () => {
+  console.log("connected");
+});
